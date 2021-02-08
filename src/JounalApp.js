2c5345398341e035195b9f21032d90f34eecfd90
Joansaro <joansaro.sant@gmail.com>
2021-02-08 11:31:03 -0600

    Primer commit

---
+++ src/JounalApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AppRouter } from './routers/AppRouter'
+
+export const JounalApp = () => {
+    return (
+        <div>
+            <AppRouter />
+        </div>
+    )
+}
