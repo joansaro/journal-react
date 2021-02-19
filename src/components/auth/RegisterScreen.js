0fbc2a584b096c3b2be382ddeede1c4948fbec34
Joansaro <joansaro.sant@gmail.com>
2021-02-18 18:34:15 -0600

    Validacion y gention de rutas

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,18 +1,74 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import {useDispatch, useSelector} from 'react-redux'
+import validator from 'validator';   
+import { removeError, setError } from '../../actions/ui';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 
 export const RegisterScreen = () => {
+
+    const dispatch = useDispatch();
+    const {msgError} = useSelector(state => state.ui);
+
+    console.log(msgError);
+
+    const [formValues, handleInputChange] = useForm({
+        name: 'Andresito',
+        email: 'andy@gamil.com',
+        password: '123456',
+        password2: '123456',
+    });
+
+    const{name,email,password,password2} = formValues;
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        
+        if(isFormValid()){
+            dispatch( startRegisterWithEmailPasswordName(email, password, name))
+        }
+    }
+
+    const isFormValid = () =>{
+        if( name.trim().length === 0 ){
+            dispatch( setError('Name is required') );
+            return false;
+        } else if( !validator.isEmail(email) ){
+            dispatch( setError( 'Email is required') );     
+            return false;
+        } else if( password !== password2 || password.length < 5 ) {
+            dispatch( setError('Password should be at least 6 characters and match each other'));
+            return false;
+        }
+
+        dispatch(removeError());
+        return true;
+    }
+
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
-            <form>
+            <form onSubmit={handleRegister}>
+
+                {
+                    msgError &&
+                    (
+                        <div className="auth__alert-error">
+                            {msgError}
+                        </div>
+                    )
+                }
+
                 <input
                     type="name"
                     placeholder="Name"
                     name="name"
                     className="auth__input"
                     autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
                 />
                 <input
                     type="text"
@@ -20,13 +76,17 @@ export const RegisterScreen = () => {
                     name="email"
                     className="auth__input"
                     autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
                 />
 
                 <input
                     type="password"
                     placeholder="password"
-                    name="Password"
+                    name="password"
                     className="auth__input"
+                    value={password}
+                    onChange={handleInputChange}
                 />
 
                 <input
@@ -34,6 +94,8 @@ export const RegisterScreen = () => {
                     placeholder="Confirm password"
                     name="password2"
                     className="auth__input"
+                    value={password2}
+                    onChange={handleInputChange}
                 />
 
                 <button
